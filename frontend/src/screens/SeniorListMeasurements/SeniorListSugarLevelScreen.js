9cb1eae5d5414db31982b306f8a032c0eef99e91
Zuzanna Adamiuk <z.adamiuk@stud.elka.pw.edu.pl>
2022-01-25 15:16:57 +0100

    REstructured code

---
+++ frontend/src/screens/SeniorListMeasurements/SeniorListSugarLevelScreen.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { styles } from './SeniorListMeasurements.styles'
+import { GetSeniorSugarList } from '../../../services'
+import { View, ScrollView, Pressable, Text } from 'react-native'
+import { ProfileCard } from '../../components/organisms/ProfileCard/ProfileCard'
+import { SugarChart } from '../../components/organisms/BloodPressure copy/SugarGraph/SugarGraph'
+import { SugarTableComponent } from '../../components/organisms/BloodPressure copy/SugarTable/SugarTable'
+import { capitalizeFirstLetter } from '../../../config/helpers'
+import { useIsFocused } from '@react-navigation/native'
+import { useEffect } from 'react'
+import { MonthPicker } from '../../components/organisms/MonthPicker/MonthPicker'
+
+export const SeniorListSugarScreen = ({ route, navigation }) => {
+  const { id, name, email } = route.params
+  let seniorName = JSON.stringify(name).replace(/["]+/g, '')
+  const seniorEmail = JSON.stringify(email).replace(/["]+/g, '')
+  const seniorId = JSON.stringify(id).replace(/["]+/g, '')
+
+  seniorName = capitalizeFirstLetter(seniorName)
+
+  const [data, setData] = useState([])
+  const isFocused = useIsFocused()
+
+  useEffect(() => {
+    GetSeniorSugarList(seniorId, setData, navigation)
+  }, [isFocused])
+
+  const today = new Date()
+  const [month, setMonth] = useState(0)
+  const [expanded, setExpanded] = useState(false)
+  const [visible, setVisible] = useState(false)
+
+  const setMonthToDisplay = (number) => {
+    setMonth(number)
+    setExpanded(false)
+  }
+  return (
+    <ScrollView>
+      <View style={styles.view}>
+        <ProfileCard name={seniorName} email={seniorEmail} />
+        {MonthPicker(expanded, setMonthToDisplay, setExpanded)}
+        {visible ? (
+          <View style={styles.buttonContainer}>
+            <Pressable
+              onPress={() => setVisible(!visible)}
+              style={styles.buttonVisible}>
+              <Text style={styles.buttonTextVisible}>Ukryj wykresy</Text>
+            </Pressable>
+          </View>
+        ) : (
+          <View style={styles.buttonContainer}>
+            <Pressable
+              onPress={() => setVisible(!visible)}
+              style={styles.buttonInvisible}>
+              <Text style={styles.buttonTextInvisible}>Pokaż wykresy</Text>
+            </Pressable>
+          </View>
+        )}
+
+        {visible ? <SugarChart data={data} month={month} /> : <></>}
+        <SugarTableComponent
+          data={data}
+          navigation={navigation}
+          month={month}
+          supervised={true}
+        />
+      </View>
+    </ScrollView>
+  )
+}
